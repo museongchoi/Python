557c3d604eef73d74a3359c13d224a05f7d1ee1d
museongchoi <97529960+museongchoi@users.noreply.github.com>
2024-03-08 16:57:03 +0900

    name_ch

---
+++ pr_basic training/pr_print/print_2_f.py
@@ -0,0 +1,7 @@
+# 덧셈식 출력하기
+a, b = map(int, input().strip().split(' '))
+print(f"{a} + {b} =", a + b)
+
+# a, b = map(int, input().strip().split(' '))
+# str = str(a) + " + " + str(b) + " ="
+# print(str, a + b)
