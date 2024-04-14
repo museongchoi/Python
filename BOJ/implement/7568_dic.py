154cc8e3bb281c526501123325553ef2a3236fb5
museongchoi <97529960+museongchoi@users.noreply.github.com>
2024-04-14 23:07:19 +0900

    Update 7568_dic.py

--- BOJ/implement/7568_dic.py
+++ BOJ/implement/7568_dic.py
@@ -6,5 +6,18 @@ for _ in range(n):
     x, y = map(int, sys.stdin.readline().split())
     dic[x] = y
 
-for p in dic:
-    for 
+dic_s = sorted(dic.items())
+
+for i in range(n):
+    k = dic_s[i][0]
+    v1 = dic_s[i][1]
+
+    cnt = 0
+    for j in range(i, n):
+        v2 = dic_s[j][1]
+        if v1 < v2:
+            cnt += 1
+
+    dic[k] = cnt + 1
+
+print(list(dic.values()))
