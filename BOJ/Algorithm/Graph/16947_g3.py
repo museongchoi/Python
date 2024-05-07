ad8a358d6295ca40357db446201209fc71d025f4
museongchoi <97529960+museongchoi@users.noreply.github.com>
2024-05-08 02:11:39 +0900

    Update 16947_g3.py

--- BOJ/Algorithm/Graph/16947_g3.py
+++ BOJ/Algorithm/Graph/16947_g3.py
@@ -8,21 +8,20 @@ def DFS(x, st_i, cnt):
     if ans:
         return
 
-    visited[x] = True
+    visited[x] = 0
     for k in arr[x]:
-        if visited[k] == False:
+        if visited[k] == -1: # 방문 하지 않은 곳이면, 재귀 호출. cnt 증가
             DFS(k, st_i, cnt + 1)
             if not ans:
-                visited[k] = False
-        elif visited[k] == True and k == st_i and cnt >= 3:
+                visited[k] = -1
+        elif visited[k] == 0 and k == st_i and cnt >= 3:
             ans = True
             return
 
 n = int(input())
 arr = [[] for _ in range(n+1)]
-visited = [False]*(n+1)
+visited = [-1]*(n+1)
 ans = False
-count = [0 for _ in range(n)]
 
 for _ in range(n):
     a, b = map(int, input().split())
@@ -30,29 +29,29 @@ for _ in range(n):
     arr[b].append(a)
 
 for i in range(1,n+1):
-    if visited[i] == False:
+    if visited[i] == -1:
         DFS(i, i, 1)
         if ans == True:
             break
 
 from collections import deque
 def BFS(j):
-    visited[j] = True
+    visited[j] = 0
     dq = deque()
     dq.append([j, 0])
 
     while dq:
-        xx, cnt = dq.popleft()
-        for k in arr[xx]:
-            if visited[k] == True:
+        x_num, cnt = dq.popleft()
+        for k in arr[x_num]:
+            if visited[k] == 0:
                 # j : 처음 값을 인덱스로 사용하여 거리를 저장한다.
-                count[j-1] = count[k-1] + 1
+                visited[j] = cnt + 1
                 break
-            elif visited[k] == False:
-                dq.append([k, cnt+1])
-
+            elif visited[k] != 0 or visited[k] == -1:
+                dq.append([k, cnt + 1])
+print(visited)
 for j in range(1, n+1):
-    if visited[j] == False:
+    if visited[j] == -1:
         BFS(j)
-    print(count[j-1], end=' ')
+    print(visited[j], end=' ')
 
