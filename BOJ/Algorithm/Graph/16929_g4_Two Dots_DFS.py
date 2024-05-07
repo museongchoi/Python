b7602c304c49c0d5d534fabd8491e3ec3f1a06a1
museongchoi <97529960+museongchoi@users.noreply.github.com>
2024-05-07 16:19:09 +0900

    Update 16929_g4_Two Dots_DFS.py

--- BOJ/Algorithm/Graph/16929_g4_Two Dots_DFS.py
+++ BOJ/Algorithm/Graph/16929_g4_Two Dots_DFS.py
@@ -15,10 +15,11 @@ def DFS(x, y, cnt, st_x, st_y):
         nx = x + dx[k]
         ny = y + dy[k]
         if 0 <= nx < n and 0 <= ny < m:
-            if cnt >= 4 and nx == st_x and ny == st_y:
-                ans = True
-                return
-            if maps[nx][ny] == maps[st_x][st_y] and visited[nx][ny] == False:
+            if visited[nx][ny]:
+                if cnt >= 4 and nx == st_x and ny == st_y:
+                    ans = True
+                    return
+            elif maps[nx][ny] == maps[st_x][st_y]:
                 DFS(nx, ny, cnt+1, st_x, st_y)
     visited[x][y] = False
 
@@ -29,12 +30,104 @@ ans = False  # False : 사이클이 아님, True : 사이클을 발견함.
 
 for i in range(n):
     for j in range(m):
-        st_i = i
-        st_j = j
-        DFS(i, j, 1, st_i, st_j)
-        if ans:
-            print('YES')
-            sys.exit(0)
+        if not visited[i][j]:
+            st_i = i
+            st_j = j
+            DFS(i, j, 1, st_i, st_j)
+            if ans:
+                print('Yes')
+                sys.exit(0)
 
-else:
-    print('NO')
+print('No')
+
+# 방법2
+# Two Dots
+# import sys
+# input = sys.stdin.readline
+#
+# dx = [-1, 0, 1, 0]
+# dy = [0, -1, 0, 1]
+#
+# def DFS(x, y, cnt, st_x, st_y):
+#     global ans
+#     if ans:
+#         return
+#
+#     visited[x][y] = True
+#     for k in range(4):
+#         nx = x + dx[k]
+#         ny = y + dy[k]
+#         if 0 <= nx < n and 0 <= ny < m:
+#             if cnt >= 4 and nx == st_x and ny == st_y:
+#                 ans = True
+#                 return
+#             if maps[nx][ny] == maps[st_x][st_y] and visited[nx][ny] == False:
+#                 DFS(nx, ny, cnt+1, st_x, st_y)
+#     visited[x][y] = False
+#
+# n, m = map(int, input().split())
+# maps = [list(map(str, input().strip())) for _ in range(n)]
+# visited = [[False]*m for _ in range(n)]
+# ans = False  # False : 사이클이 아님, True : 사이클을 발견함.
+#
+# for i in range(n):
+#     for j in range(m):
+#         st_i = i
+#         st_j = j
+#         DFS(i, j, 1, st_i, st_j)
+#         if ans:
+#             print('Yes')
+#             sys.exit(0)
+#
+# else:
+#     print('No')
+
+# 방법 1
+# Two Dots
+# import sys
+#
+# input = sys.stdin.readline
+#
+# dx = [-1, 0, 1, 0]
+# dy = [0, -1, 0, 1]
+# ans = False  # False : 사이클이 아님, True : 사이클을 발견함.
+#
+#
+# def DFS(x, y, cnt, st_x, st_y):
+#     global ans
+#     if cnt >= 4 and x == st_x and y == st_y:
+#         ans = True
+#         return
+#
+#     visited[x][y] = True
+#
+#     for k in range(4):
+#         nx = x + dx[k]
+#         ny = y + dy[k]
+#         if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == maps[st_x][st_y]:
+#             if visited[nx][ny] == False:
+#                 DFS(nx, ny, cnt + 1, st_x, st_y)
+#
+#             elif cnt >= 4 and nx == st_x and ny == st_y:
+#                 ans = True
+#                 return
+#     visited[x][y] = False
+#
+#
+# n, m = map(int, input().split())
+# maps = [list(map(str, input().strip())) for _ in range(n)]
+# visited = [[False] * m for _ in range(n)]
+# cnt = 1
+#
+# for i in range(n):
+#     for j in range(m):
+#         if visited[i][j] == False:
+#             st_i = i
+#             st_j = j
+#             DFS(i, j, cnt, st_i, st_j)
+#         if ans:
+#             print('Yes')
+#             exit()
+#
+# else:
+#     print('No')
