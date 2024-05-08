a5cb9fd2be15e1f14791e8eb6a65e02f7de8e7ba
museongchoi <97529960+museongchoi@users.noreply.github.com>
2024-05-08 20:14:53 +0900

    2024_05_08

--- BOJ/Algorithm/Graph/16947_g3.py
+++ BOJ/Algorithm/Graph/16947_g3.py
@@ -1,27 +1,44 @@
 # 서울 지하철 2호선
-
 import sys
+from collections import deque
 input = sys.stdin.readline
 sys.setrecursionlimit(100000)
-def DFS(x, st_i, cnt):
+def DFS(idx, st_idx, cnt):
     global ans
-    if ans:
+    if st_idx == idx and cnt >= 2:
+        ans = True
         return
 
-    visited[x] = 0
-    for k in arr[x]:
-        if visited[k] == -1: # 방문 하지 않은 곳이면, 재귀 호출. cnt 증가
-            DFS(k, st_i, cnt + 1)
-            if not ans:
-                visited[k] = -1
-        elif visited[k] == 0 and k == st_i and cnt >= 3:
-            ans = True
-            return
+    visited[idx] = True
+    for k in arr[idx]:
+        if visited[k] == False: # 방문 하지 않은 곳이면, 재귀 호출. cnt 증가
+            DFS(k, st_idx, cnt + 1)
+        elif k == st_idx and cnt >= 2:
+            DFS(k, st_idx, cnt)
 
-n = int(input())
-arr = [[] for _ in range(n+1)]
-visited = [-1]*(n+1)
-ans = False
+def BFS():
+    global ck
+    dq = deque()
+    for i in range(1, n+1):
+        if cycle_st[i]:
+            ck[i] = 0
+            dq.append(i)
+
+    while dq:
+        x_idx = dq.popleft()
+        for k in arr[x_idx]:
+            # -1 인 값을 큐에 넣는다.
+            if ck[k] == -1:
+                dq.append(k)
+                ck[k] = ck[x_idx] + 1
+
+    for j in ck[1:]:
+        print(j, end=' ')
+
+n = int(input()) # 역 개수 입력받기
+arr = [[] for _ in range(n+1)]  # 역과 역을 연결하는 구간의 정보
+cycle_st = [False]*(n+1) # 순환역 표시하는 전체 역
+ck = [-1] * (n+1) # 거리 저장
 
 for _ in range(n):
     a, b = map(int, input().split())
@@ -29,29 +46,10 @@ for _ in range(n):
     arr[b].append(a)
 
 for i in range(1,n+1):
-    if visited[i] == -1:
-        DFS(i, i, 1)
-        if ans == True:
-            break
+    visited = [False] * (n + 1)
+    ans = False
+    DFS(i, i, 0)
+    if ans:
+        cycle_st[i] = True
 
-from collections import deque
-def BFS(j):
-    visited[j] = 0
-    dq = deque()
-    dq.append([j, 0])
-
-    while dq:
-        x_num, cnt = dq.popleft()
-        for k in arr[x_num]:
-            if visited[k] == 0:
-                # j : 처음 값을 인덱스로 사용하여 거리를 저장한다.
-                visited[j] = cnt + 1
-                break
-            elif visited[k] != 0 or visited[k] == -1:
-                dq.append([k, cnt + 1])
-print(visited)
-for j in range(1, n+1):
-    if visited[j] == -1:
-        BFS(j)
-    print(visited[j], end=' ')
-
+BFS()
---
+++ BOJ/Algorithm/Tree/1991_s1.py
@@ -0,0 +1,40 @@
+# 트리 순회
+# 전위 순회한 결과 : ABDCEFG // (루트) (왼쪽 자식) (오른쪽 자식)
+# 중위 순회한 결과 : DBAECFG // (왼쪽 자식) (루트) (오른쪽 자식)
+# 후위 순회한 결과 : DBEGFCA // (왼쪽 자식) (오른쪽 자식) (루트)
+
+import sys
+input = sys.stdin.readline
+n = int(input())
+
+tree = {}
+for _ in range(n):
+    root, left, right = map(str, input().split())
+    tree[root] = [left, right]
+
+
+def preorder(root):
+    if root != '.':
+        print(root, end='')
+        preorder(tree[root][0])
+        preorder(tree[root][1])
+
+
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])
+        print(root, end='')
+        inorder(tree[root][1])
+
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0])
+        postorder(tree[root][1])
+        print(root, end='')
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
+
