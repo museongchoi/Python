042763514de6bed4971c39957790982b7763a194
museongchoi <97529960+museongchoi@users.noreply.github.com>
2024-05-14 00:04:27 +0900

    2024_05_14

--- BOJ/implement/13458_b2.py
+++ BOJ/implement/13458_b2.py
@@ -5,9 +5,18 @@ input = sys.stdin.readline
 
 n = int(input())
 test_num = list(map(int, input().split()))
-b, c = map
+b, c = map(int, input().split())
 # 총 감독관은 최소 1명이 각 시험장에 있어야 한다.
 # 각 시험장 마다 있는 감독관 최소 수를 구하라.
-
+cnt = 0
 for test in test_num:
-    if test//b 
+    cnt += 1
+    test -= b
+
+    if test > 0:
+        if test%c == 0:
+            cnt += test//c
+        else:
+            cnt += test//c + 1
+
+print(cnt)
---
+++ BOJ/implement/14501_s3.py
@@ -0,0 +1,8 @@
+# 퇴사
+# bottom up
+import sys
+input = sys.stdin.readline
+n = int(input())
+li = [list(map(int, input().split())) for _ in range(n)]
+
+print(li)
