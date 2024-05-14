dd5a65ae9aa2e34687e0f39cafbd2aae509b36e5
museongchoi <97529960+museongchoi@users.noreply.github.com>
2024-05-14 19:01:29 +0900

    2024_05_14

--- BOJ/implement/14501_s3_BottomUp.py
+++ BOJ/implement/14501_s3_BottomUp.py
@@ -1,15 +1,17 @@
 # 퇴사
+# n+1 되는 날 퇴사.
+# n일 동안 많은 상담을 진행. li는 상담을 완료하는 기간과 금액 입력, 상담이 완료 되기 전까지는 다른 상담 진행 불가.
 # bottom up
+
 import sys
 input = sys.stdin.readline
 n = int(input())
 li = [list(map(int, input().split())) for _ in range(n)]
-dp = [0] * (n+1)
+dp = [0 for _ in range(n+1)]
 
 for day in range(n):
     for j in range(day+li[day][0], n+1):
         if dp[j] < dp[day] + li[day][1]:
             dp[j] = dp[day] + li[day][1]
 
-print(dp)
-print(dp[-1]) # 최종 가장 마지막 값 출력
+print(dp[-1])
--- BOJ/implement/14501_s3_TopDown.py
+++ BOJ/implement/14501_s3_TopDown.py
@@ -1,1 +1,22 @@
 # 퇴사
+# top down
+
+# 재귀 사용
+import sys
+input = sys.stdin.readline
+n = int(input())
+li = [list(map(int, input().split())) for _ in range(n)]
+dp = [0 for _ in range((n+1))]
+ans = 0
+def find(day, s):
+    global ans
+    if day > n:
+        return
+    if day == n:
+        ans = max(ans, s)
+        return
+    find(day+li[day][0], s+li[day][1])
+    find(day+1, s)
+
+find(0,0)
+print(ans)
